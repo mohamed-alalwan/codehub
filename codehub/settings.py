45509a5130bb9481f7a24050567158545338b970
mohamedalalwan <mohamedalalwan98@gmail.com>
2023-03-16 13:10:37 +0300

    rebase

--- codehub/settings.py
+++ codehub/settings.py
@@ -122,6 +122,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+#Specify Login Redirect
+LOGIN_REDIRECT_URL = '/'
+
+#Specify Login Redirect
+LOGOUT_REDIRECT_URL = '/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
