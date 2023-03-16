95ef259dd3de192f8fd96513cc1fc75fbea3813d
Aref Rabea <aref.h.rabea@gmail.com>
2023-03-16 12:39:54 +0300

    Added models

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,4 +2,9 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home, name='home'),
+    path('question/index', views.question_index, name='question_index'),
+    path('question/create/', views.CreateQuestion.as_view() , name='create_question'),
+    path('question/<int:question_id>/', views.question_detail, name='question_detail'),
+    path('question/<int:pk>/update/', views.UpdateView.as_view(), name='update_question'),
+    path('question/<int:pk>/delete/', views.QuestionDelete.as_view(), name='question_delete')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,36 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from .models import Question
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def question_index(request):
+    questions = Question.objects.all()
+    return render(request,'question/question_index.html', {'questions': questions})
+
+def question_detail(request, question_id):
+    cat = Question.objects.get(id=question_id)
+
+
+
+
+# =======================Classes========================
+
+class CreateQuestion(CreateView):
+    model= Question
+    fields = '__all__'
+    
+
+class QuestionUpdate(UpdateView):
+    model = Question
+    fields = '__all__'
+
+class QuestionDelete(DeleteView):
+    model = Question
+    success_url = '/'
+
+class QuestionDetail(DetailView):
+    model = Question
