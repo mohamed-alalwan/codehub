6784ac1fa42baa514feeb14579122fdb352b94fe
Aref Rabea <aref.h.rabea@gmail.com>
2023-03-18 10:00:34 +0300

    completed reply crud

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,7 @@
 
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
-from .models import Question, Category, Answer
+from .models import Question, Category, Answer, Reply
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
@@ -55,7 +55,15 @@ class QuestionDelete(DeleteView):
 
 def answer_index(request):
     answers = Answer.objects.all()
-    return render(request,'question/question_index.html', {'answers': answers})
+    return render(request,'answer/answer_index.html', {'answers': answers})
+
+def answer_detail(request, answer_id):
+    answer= Answer.objects.get(id=answer_id)
+    
+    return render(request, 'answer/answer_detail.html', {
+        'answer': answer,
+        
+        })
 
 
 class CreateAnswer(CreateView):
@@ -69,7 +77,37 @@ class AnswerUpdate(UpdateView):
 
 class AnswerDelete(DeleteView):
     model = Answer
+    success_url = '/answer/'
+
+
+
+# =======================Reply Section========================
+
+def reply_index(request):
+    replies = Reply.objects.all()
+    return render(request,'reply/reply_index.html', {'replies': replies})
+
+def reply_detail(request, reply_id):
+    reply = Reply.objects.get(id=reply_id)
     
+    return render(request, 'reply/reply_detail.html', {
+        'reply': reply,
+        
+        })
+
+
+class CreateReply(CreateView):
+    model= Reply
+    fields = '__all__'
+    
+
+class ReplyUpdate(UpdateView):
+    model = Reply
+    fields = '__all__'
+
+class ReplyDelete(DeleteView):
+    model = Reply
+    success_url = '/reply/'
 
 
 # =======================Sign Up Section========================
