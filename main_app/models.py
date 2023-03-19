f9cc9750215da43395c5a0d007ce7c80c890d721
Skhalfan <123301102+Skhalfan@users.noreply.github.com>
2023-03-19 09:41:55 +0300

    icon_code

--- main_app/models.py
+++ main_app/models.py
@@ -7,6 +7,7 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
+    icon_code = models.CharField(max_length=50, null=True)
 
     def __str__(self):
         return self.name
